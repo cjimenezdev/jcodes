331146b46f20dcd5c52e7553d354b58f824c34d9
Carlos Jiménez <carlosjimenez32077@gmail.com>
2022-07-06 00:26:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                             alt="page-not-found">
                     </div>
                     <div class="d-flex justify-content-center align-items-center">
-                        <a type="button" href="#" class="btn rounded-4 bg-primary text-white"><i class="fi fi-sr-home"></i> Return Home</a>
+                        <a type="button" href="#" class="btn rounded-4 bg-primary text-white-50"><i class="fi fi-sr-home"></i> Return Home</a>
                     </div>
                 </div>
             </div>
